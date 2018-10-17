50f93edb03846689df363e5c8363fab84e8db077
Арман Сауенов <@local.itstep>
2018-10-17 21:22:47 +0600

    Добавьте файлы проекта.

---
+++ Pattern1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Pattern1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread thr = new Thread(() => {
+                SingletoneExample s = SingletoneExample.Dat();
+                s.CountOne();
+            });
+
+            thr.Start();
+
+            Thread thr2 = new Thread(() => {
+                SingletoneExample s = SingletoneExample.Dat();
+                s.CountOne();
+            });
+
+            thr2.Start();
+        }
+    }
+}
---
+++ Pattern1/SingletoneExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern1
+{
+    public class SingletoneExample
+    {
+
+        private SingletoneExample()
+        {
+
+        }
+
+        public void CountOne()
+        {
+            lock(dat)
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+
+        private static SingletoneExample dat;
+        public static SingletoneExample Dat ()
+        {
+            if (dat == null)
+                dat = new SingletoneExample();
+
+                return dat;
+        }
+    }
+}
